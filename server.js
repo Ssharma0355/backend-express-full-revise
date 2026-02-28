75b2a25c585f458a53e1336514f0d7be70d2c314
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-28 09:06:32 +0530

    response

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const app = express();
 
+app.get("/",(req,res)=>{
+   res.send("hi sachin")
+})
+
 app.listen(8080,()=>{
     console.log("server running")
 })
