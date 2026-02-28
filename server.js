17ea0d8c7a91e551b4e3795ccd619423e583e16d
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-28 09:12:45 +0530

    baiscs started

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require("express")
 
 const app = express();
 
+const person = [
+    {
+        name:"Sachin",
+        email:"Per@gmail.com",
+    },
+     {
+        name:"Sachin",
+        email:"Per@gmail.com",
+    },
+     {
+        name:"Sachin",
+        email:"Per@gmail.com",
+    },
+
+] 
+
 app.get("/",(req,res)=>{
-   res.send("hi sachin")
+   res.send(person)
+})
+
+app.get("/about",(req,res)=>{
+    res.send("This os about page")
 })
 
 app.listen(8080,()=>{
